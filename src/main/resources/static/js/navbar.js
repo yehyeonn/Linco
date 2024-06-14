1dd1fdb09fb65dd0a1fc5e6408ce9ff03fe46525
Han-DDong <jungwoo03173288@gmail.com>
2024-06-14 12:23:23 +0900

    # navbar 반응형 수정 # header.html 수정 # common.css 수정 # navbar.js 삭제

--- src/main/resources/static/js/navbar.js
+++
@@ -1,9 +0,0 @@
-// 메뉴 토글 버튼 작동
-const toogleBtn = document.querySelector(".navbar_tooglebtn");
-const menu = document.querySelector(".nav_menu");
-const icons = document.querySelector(".nav_icon");
-
-toogleBtn.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    icons.classList.toggle("active");
-});
