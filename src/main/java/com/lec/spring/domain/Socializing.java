cd918296ea3656d11b0cb51f2c7514c27fe31632
yehyeonn <yehyeon99@naver.com>
2024-06-14 10:14:18 +0900

    Socializing 작성

--- src/main/java/com/lec/spring/domain/Socializing.java
+++ src/main/java/com/lec/spring/domain/Socializing.java
@@ -5,10 +5,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Socializing {
-
+    private Long id;
+    private String socializing_title;
+    private String category;
+    private String detail_category;
+    private String address;
+    private LocalDate meeting_date;
+    private LocalTime meeting_time;
+    private Integer limit_num;
+    private String content;
+    private Long total_price;
+    private String img;
+    private LocalDateTime regdate;
 }
